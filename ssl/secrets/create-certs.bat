78b564e98c688fb480b56ea980de2d89660fbf0a
Abhishek Joshi <abhishek2185@gmail.com>
2022-07-24 13:52:34 +0530

    SSL changes

--- ssl/secrets/create-certs.bat
+++ ssl/secrets/create-certs.bat
@@ -2,6 +2,13 @@
 echo "Generate CA key"
 openssl req -new -x509 -keyout root-ca.key -out root-ca.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
+:: Kafka Client
+openssl req -newkey rsa:2048 -nodes -keyout kafka.client.key -out kafka_client.csr -subj "/CN=kafkaclient.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US"
+openssl req -passin "pass:confluent" -passout "pass:confluent" -key kafka.client.key -new -out kafka.client.req -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US'
+openssl req -passin "pass:confluent" -passout "pass:confluent" -key kafka.client.key -new -out kafka.client.csr -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US'
+openssl x509 -req -CA root-ca.crt -CAkey root-ca.key -in kafka.client.req -out kafka-ca1-signed.pem -days 9999 -CAcreateserial -passin "pass:confluent"
+openssl x509 -req -CA root-ca.crt -CAkey root-ca.key -in kafka.client.csr -out kafka.client.crt -days 365 -CAcreateserial -passin "pass:confluent"
+
 echo "Server"
 :: Server
 keytool -genkey -noprompt -alias server -dname "CN=server.test.confluent.io, OU=TEST, O=CONFLUENT, L=PaloAlto, S=Ca, C=US" -keystore kafka.server.keystore.jks -keyalg RSA -storepass confluent -keypass confluent
@@ -9,9 +16,7 @@ keytool -genkey -noprompt -alias server -dname "CN=server.test.confluent.io, OU=
 echo "Create CSR, sign the key and import back into keystore"
 :: Create CSR, sign the key and import back into keystore
 keytool -keystore kafka.server.keystore.jks -alias server -certreq -file server.csr -storepass confluent -keypass confluent
-
-
-openssl x509 -req -CA root-ca.crt -CAkey root-ca.key -in server.csr -out server-ca1-signed.crt -days 9999 -CAcreateserial -passin pass:confluent
+openssl x509 -req -CA root-ca.crt -CAkey root-ca.key -in server.csr -out server-ca1-signed.crt -days 9999 -CAcreateserial -passin "pass:confluent"
 
 keytool -keystore kafka.server.keystore.jks -alias CARoot -import -file root-ca.crt -storepass confluent -keypass confluent
 
@@ -39,4 +44,4 @@ keytool -keystore kafka.client.truststore.jks -alias CARoot -import -file root-c
 ::.Net Client require certificate
 
 openssl req -newkey rsa:2048 -nodes -keyout kafka_client.key -out kafka_client.csr -subj "/CN=kafkaclient.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US"
-openssl x509 -req -CA root-ca.crt -CAkey root-ca.key -in kafka_client.csr -out kafka_client.crt -days 365 -CAcreateserial
+openssl x509 -req -CA root-ca.crt -CAkey root-ca.key -in kafka_client.csr -out kafka_client.crt -days 365 -CAcreateserial -passin "pass:confluent"
